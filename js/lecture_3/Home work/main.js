ab421ebf37f22e3d99c51f54163c7d2353d1cc1b
dannaleevan <dannaleevan@gmail.com>
2021-12-01 01:40:25 +0200

    HW_3

--- js/lecture_3/Home work/main.js
+++ js/lecture_3/Home work/main.js
@@ -5,28 +5,26 @@ const c = 8;
 function makeSomeCoolStaff() {
   var c = 5;
 
-  console.log(a); //1) a = 1;
-  console.log(b); //2) b = 2;
-  console.log(c); //3) c = 8;
+  console.log(a); //1) a = 1;-7
+  console.log(b); //2) b = 2;-und
+  console.log(c); //3) c = 8;-5
 
   if (true) {
     a = 3;
     var b =4;
 
-    console.log(a); //4) a=undefined;
-    console.log(b); //5) b=2;
-    console.log(c); //6) c=mistake;
+    console.log(a); //4) a=3;+
+    console.log(b); //5) b=4;+
+    console.log(c); //6) c=mistake;-5
   }
 
-  console.log(a); //7) a=undefined;
-  console.log(b); //8) b=2;
-  console.log(c); //9) c=mistake;
+  console.log(a); //7) a=1;-3
+  console.log(b); //8) b=4;+
+  console.log(c); //9) c=5;+
 }
 
 a = 7;
 
 makeSomeCoolStaff ();
 
-console.log(a); //10) a=undefined;
-console.log(b); //11) b=2;
-console.log(c); //12) c=mistake;
+
