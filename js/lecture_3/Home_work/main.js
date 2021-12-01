2f9b133ff63e14a819595573201cceee724c2a00
dannaleevan <dannaleevan@gmail.com>
2021-12-01 02:59:37 +0200

    HW finished

--- js/lecture_3/Home_work/main.js
+++ js/lecture_3/Home_work/main.js
@@ -7,31 +7,31 @@ const c = 8;
 function makeSomeCoolStaff() {
   var c = 5;
 
-  console.log(a); //1) a = 1;-7
-  console.log(b); //2) b = 2;-und
+  console.log(a); //1) a = 7; (1)
+  console.log(b); //2) b = undefined;(2)
   console.log(c); //3) c = 5;
 
   if (true) {
     a = 3;
     var b =4;
 
-    console.log(a); //4) a=3;+
-    console.log(b); //5) b=4;+
-    console.log(c); //6) c=5;
+    console.log(a); //4) a = 3;
+    console.log(b); //5) b = 4;
+    console.log(c); //6) c = 5;
   }
 
-  console.log(a); //7) a=1;-3
-  console.log(b); //8) b=4;+
-  console.log(c); //9) c=5;
+  console.log(a); //7) a = 3; (1)
+  console.log(b); //8) b = 4;
+  console.log(c); //9) c = 5;
 }
 
 a = 7;
 
 makeSomeCoolStaff();
 
-console.log(a); //10) a=1;-3
-console.log(b); //11) b=4;+ 2
-console.log(c); //12) c=5;+ 8
+console.log(a); //10) a = 3; (1)
+console.log(b); //11) b = 2; (4)
+console.log(c); //12) c = 8; (5)
 
 //Task 2. Dynamic greeting
 
