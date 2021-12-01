048831fafe36d42f7271b39cf8c28769e6bbee12
dannaleevan <dannaleevan@gmail.com>
2021-12-01 02:55:13 +0200

    HW_3 finished

--- js/lecture_3/Home_work/main.js
+++ js/lecture_3/Home_work/main.js
@@ -1,3 +1,5 @@
+//Task 1.
+
 let a = 1;
 let b = 2;
 const c = 8;
@@ -7,7 +9,7 @@ function makeSomeCoolStaff() {
 
   console.log(a); //1) a = 1;-7
   console.log(b); //2) b = 2;-und
-  console.log(c); //3) c = 8;-5
+  console.log(c); //3) c = 5;
 
   if (true) {
     a = 3;
@@ -15,16 +17,40 @@ function makeSomeCoolStaff() {
 
     console.log(a); //4) a=3;+
     console.log(b); //5) b=4;+
-    console.log(c); //6) c=mistake;-5
+    console.log(c); //6) c=5;
   }
 
   console.log(a); //7) a=1;-3
   console.log(b); //8) b=4;+
-  console.log(c); //9) c=5;+
+  console.log(c); //9) c=5;
 }
 
 a = 7;
 
-makeSomeCoolStaff ();
+makeSomeCoolStaff();
 
+console.log(a); //10) a=1;-3
+console.log(b); //11) b=4;+ 2
+console.log(c); //12) c=5;+ 8
 
+//Task 2. Dynamic greeting
+
+function greeting(name) {
+  alert('Hello, ' + name + '! How are you?');
+}
+
+greeting("Daria");
+
+//Task 3. My lecture object
+
+const lecture = {
+  boolean: true,
+  null: null,
+  undefined: undefined,
+  number: 3,
+  bigInt: 9007199254740991n,
+  string: 'Hi',
+  symbol: 'foo',
+}
+
+console.log(`We were studing ${lecture.bigInt} during last lecture`);
