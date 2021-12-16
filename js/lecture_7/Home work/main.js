039bf21937d18f59b0d616da9f4b76c2ba9c624c
dannaleevan <dannaleevan@gmail.com>
2021-12-16 02:08:18 +0200

    task2 complited

--- js/lecture_7/Home work/main.js
+++ js/lecture_7/Home work/main.js
@@ -1,19 +1,41 @@
-//task 1
+// //task 1
 
-//1)
-const inputString = 'But I must explain to you how all this mistaken idea.';
-const inputString2 = inputString.toLowerCase();
-const findVowelsCount = inputString2 => Array.from(inputString2)
-  .filter(letter => 'aeiou'.includes(letter)).length;
-console.log(findVowelsCount(inputString2));
-
+// //1)
+// const inputString = 'But I must explain to you how all this mistaken idea.';
+// const inputString2 = inputString.toLowerCase();
+// const findVowelsCount = inputString2 => Array.from(inputString2)
+//   .filter(letter => 'aeiou'.includes(letter)).length;
+// console.log(findVowelsCount(inputString2));
 
 //2)
-const inputString3 = 'But I must explain to you how all this mistaken idea.';
-function findVowelsCount2(inputString3) {
-  return inputString3.match(/[aeiou]/gi).length;
-}
-console.log(findVowelsCount2(inputString3));
+// const inputString = 'But I must explain to you how all this mistaken idea.';
+// function findVowelsCount(inputString) {
+//   const inputString2 = inputString.toLowerCase();
+//   const arrFromStr = inputString2.split('');
+//   const mas2 = arrFromStr.filter(function (letter) {
+//     return 'aeiouy'.includes(letter)});
+//     return mas2.length;
+// }
+// console.log(findVowelsCount(inputString));
+    
+// // 3)
+// const inputString3 = 'But I must explain to you how all this mistaken idea.';
+// function findVowelsCount2(inputString3) {
+//   return inputString3.match(/[aeiou]/gi).length;
+// }
+// console.log(findVowelsCount2(inputString3));
 
 //task 2
+
+function joinObjects(name1, name2) {
+  const students = [];
+  const student1 = {name1: 'Hanna', age: 17 };
+  const student2 = {name2: 'Ivan', age: 12};
+  const changeSt1 = Object.entries(student1);
+  const changeSt2 = Object.entries(student2);
+  const push1 = students.push(changeSt1);
+  const push2 = students.push(changeSt2);
+  return students;
+}
+console.log(joinObjects('name2', 'name2'));
 
